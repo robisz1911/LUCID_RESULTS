044c477ce7b01072e59fa60926b486e171df5395
robisz1911 <robisz1911@gmail.com>
2019-09-04 23:58:12 +0200

    flowers_20cutoff_50epochs_3visualization

---
+++ RESULTS/PICTURES/flowers_20cutoff_50epochs_mixed4f_png/vis_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+######  NOTE : before running the script, delete pictures_x folder ###################################################
+
+mkdir flowers_20cutoff_50epochs_mixed4f_png
+cp merge.py flowers_20cutoff_50epochs_mixed4f_png/merge.py
+
+#sed -i 's/nb_epoch = .*/nb_epoch = 5/' train.py    # SET : visualize after every x'th epochs
+#sed -i 's/COLUMNS = .*/COLUMNS = 15/' vis.py       # SET : change how much neuron should be visualized       |THESE TWO-      |
+#sed -i 's/columns = .*/columns = 15/' merge.py     # SET : change how much neuron should be visualized       |MUST BE THE SAME|
+
+for i in {1..50}                                     # SET : iterate from 1 to "value" must be the number of .pb files
+do
+                                                    # each iteration we visualize->copy .png to the correct folder
+													# set the layer after grep below
+    cat googlenet-node-names | grep Mixed_4f_Branch_3_b_1x1_act/Relu | python vis.py flowers_20cutoff_50epochs/$i.pb - $i
+    mv $i.png flowers_20cutoff_50epochs_mixed4f_png
+
+done
+                                                    # finally, we cd into pictures_x folder and run merge.py ( this should be executed next to the images )
+													# NOTE : before running the script, delete pictures_x folder
+													# if u dont have your .pb files from a previous training, use train_vis_script.sh !!!
+cd flowers_20cutoff_50epochs_mixed4f_png
+python merge.py
